d89288885f6339ad99afc1b78503e3e54e30a897
alisoncleonard <alisoncleonard@gmail.com>
2019-11-07 21:08:43 -0700

    updated and tested files to build binary tree from sorted and unsorted data

--- insert_key_value_pairs.py
+++ insert_key_value_pairs.py
@@ -3,6 +3,8 @@ Main file to compare different data structures for storing key, value pairs
 """
 import argparse
 import binary_tree
+import time
+
 
 def main():
     """
@@ -13,7 +15,8 @@ def main():
     --datastructure: the datastructure to build storing desired key, value
     pairs. Choose from 'hash', 'binary_tree', or 'avl_tree'.
 
-    --dataset: a CSV file containing lines of key, value pairs to store
+    --dataset: a tab-separated txt file containing lines of key, value
+    pairs to store
 
     Returns
     -------
@@ -28,7 +31,7 @@ def main():
                         "datastructure to use. Choose from 'hash', "
                         "'binary_tree', or 'avl_tree'", required=True)
 
-    parser.add_argument('--dataset', type=str, help='Name of CSV file with key'
+    parser.add_argument('--dataset', type=str, help='Name of txt file with key'
                         ', value pairs', required=True)
 
     args = parser.parse_args()
@@ -40,16 +43,31 @@ def main():
         # call hash tables submodule
         pass
 
-    if datastructure is 'binary_tree':
+    elif datastructure == 'binary_tree':
         # call binary_tree tree function
-        pass
+        print('initialize binary tree')
+        # measure time to insert all keys in file
+        insert_t0 = time.time()
+        datatree = binary_tree.create_tree(filename)
+        insert_t1 = time.time()
+        # measure time to search for keys
+        search_t0 = time.time()
+        result = binary_tree.search(datatree, '8')
+        search_t1 = time.time()
+        print('the value of 8 is ' + str(result))
+        print('time to insert: ' + str(insert_t1 - insert_t0))
+        print('time to search: ' + str(search_t1 - search_t0))
 
-    if datastructure is 'avl_tree':
+    elif datastructure is 'avl_tree':
         # call avl_tree tree function
         pass
 
-    
+    else:
+        print('does not recognize datastructure name')
 
 
 if __name__ == '__main__':
     main()
+
+
+# python insert_key_value_pairs.py --datastructure 'binary_tree' --dataset 'commasep_testdata.csv'
--- test_trees.py
+++ test_trees.py
@@ -5,12 +5,16 @@ unit test file for binary_tree.py
 import unittest
 import binary_tree
 
+
 class TestBinaryTree(unittest.TestCase):
+
     def test_search_in_tree(self):
-        self.assertEqual(binary_tree.search(datatree,'8'), 'value5')
+        datatree = binary_tree.create_tree('tabsep_testdata.txt')
+        self.assertEqual(binary_tree.search(datatree, '30068'), '22623')
 
-
-
+    def test_search_not_in_tree(self):
+        datatree = binary_tree.create_tree('tabsep_testdata.txt')
+        self.assertEqual(binary_tree.search(datatree, '2'), None)
 
 
 if __name__ == '__main__':
