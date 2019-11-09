c95a13cf455f114e7afb37d8657d101a314a829f
alisoncleonard <alisoncleonard@gmail.com>
2019-11-08 21:40:47 -0700

    fixed avl tree module, updated main script to include number of keys argument

--- insert_key_value_pairs.py
+++ insert_key_value_pairs.py
@@ -1,12 +1,14 @@
 """
 Main file to compare different data structures for storing key, value pairs
 """
+
 import argparse
 import sys
 import binary_tree
+import avl_tree
+import time
 sys.path.insert(1, './hash-tables-alisoncleonard')
 import hash_tables as ht
-import time
 
 
 def main():
@@ -21,9 +23,13 @@ def main():
     --dataset: a tab-separated txt file containing lines of key, value
     pairs to store
 
+    --number_keys: the number of keys from dataset to read in
+
     Returns
     -------
-
+    The specified data structure containing all key, value pairs. Also prints
+    the elapsed time to insert all keys and elapsed time to search for all
+    keys.
 
     """
     parser = argparse.ArgumentParser(description='Store key, value pairs in '
@@ -37,25 +43,38 @@ def main():
     parser.add_argument('--dataset', type=str, help='Name of txt file with key'
                         ', value pairs', required=True)
 
+    parser.add_argument('--number_keys', type=int, help='Number of keys from'
+                        'dataset to read in', required=True)
+
     args = parser.parse_args()
 
     datastructure = args.datastructure
     filename = args.dataset
+    N = args.number_keys
 
     if datastructure == 'hash':
         # call hash tables submodule
+        print('initialize hash table')
         hashtable = ht.ChainedHash(10000000, ht.hash_functions.h_rolling)
         # measure time to insert all keys in file
         insert_t0 = time.time()
+        counter = 0
         for line in open(filename, 'r'):
             data = line.rstrip().split('\t')
             hashtable.add(data[0], data[1])
+            counter += 1
+            if counter == N:
+                break
         insert_t1 = time.time()
         # measure time to search for all keys
         search_t0 = time.time()
+        counter = 0
         for line in open(filename, 'r'):
             data = line.rstrip().split('\t')
             hashtable.search(data[0])
+            counter += 1
+            if counter == N:
+                break
         search_t1 = time.time()
         print('time to insert: ' + str(insert_t1 - insert_t0))
         print('time to search: ' + str(search_t1 - search_t0))
@@ -65,19 +84,40 @@ def main():
         print('initialize binary tree')
         # measure time to insert all keys in file
         insert_t0 = time.time()
-        datatree = binary_tree.create_tree(filename)
+        datatree = binary_tree.create_tree(filename, N)
         insert_t1 = time.time()
         # measure time to search for keys
         search_t0 = time.time()
-        result = binary_tree.search(datatree, '8')
+        counter = 0
+        for line in open(filename, 'r'):
+            data = line.rstrip().split('\t')
+            binary_tree.search(datatree, data[0])
+            counter += 1
+            if counter == N:
+                break
         search_t1 = time.time()
-        print('the value of 8 is ' + str(result))
         print('time to insert: ' + str(insert_t1 - insert_t0))
         print('time to search: ' + str(search_t1 - search_t0))
 
     elif datastructure == 'avl_tree':
         # call avl_tree tree function
-        pass
+        print('initialize AVL tree')
+        # measure time to insert all keys in file
+        insert_t0 = time.time()
+        datatree = avl_tree.create_AVLtree(filename, N)
+        insert_t1 = time.time()
+        # measure time to search for keys
+        search_t0 = time.time()
+        counter = 0
+        for line in open(filename, 'r'):
+            data = line.rstrip().split('\t')
+            avl_tree.search(datatree, data[0])
+            counter += 1
+            if counter == N:
+                break
+        search_t1 = time.time()
+        print('time to insert: ' + str(insert_t1 - insert_t0))
+        print('time to search: ' + str(search_t1 - search_t0))
 
     else:
         print('does not recognize datastructure name')
@@ -85,6 +125,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-# python insert_key_value_pairs.py --datastructure 'binary_tree' --dataset 'commasep_testdata.csv'
--- trees_test.sh
+++ trees_test.sh
@@ -4,9 +4,23 @@ test -e ssshtest || wget -q https://raw.githubusercontent.com/ryanlayer/ssshtest
 . ssshtest
 source ssshtest
 
-echo 'testing exception for FileNotFoundError'
 
-run test_find_file python get_column_stats.py --file_name test.txt --column_number 2
-assert_in_stdout 'Could not find test.txt'
+run test_hash python insert_key_value_pairs.py --datastructure 'hash' --dataset 'random_keys.txt' --number_keys 100
+assert_in_stdout
 assert_no_stderr
-assert_exit_code 1
+assert_exit_code 0
+
+run test_binary python insert_key_value_pairs.py --datastructure 'binary_tree' --dataset 'sorted_keys.txt' --number_keys 1000
+assert_in_stdout
+assert_no_stderr
+assert_exit_code 0
+
+run test_avl python insert_key_value_pairs.py --datastructure 'avl_tree' --dataset 'random_keys.txt' --number_keys 10000
+assert_in_stdout
+assert_no_stderr
+assert_exit_code 0
+
+run test_keys_not_in_table python insert_key_value_pairs.py --datastructure 'binary_tree' --dataset 'keys_not_in_table.txt' --number_keys 100
+assert_in_stdout
+assert_no_stderr
+assert_exit_code 0
